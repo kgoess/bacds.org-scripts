5459ae76c92b1a0972844c5e021ad1f8509a20a5
Kevin M. Goess <cpan@goess.org>
2018-10-09 12:27:51 -0700

    Use https on our urls

--- bin/serieslists.pl
+++ bin/serieslists.pl
@@ -209,7 +209,7 @@ sub generate_jsonld {
     # keys to $loc hashref are: vkey|hall|address|city|zip|comment|type
     my $loc = lookup_address_for_vkey($dbh, $venue);
 
-    my $offer_url = "http://$ENV{HTTP_HOST}$ENV{DOCUMENT_URI}";
+    my $offer_url = "https://$ENV{HTTP_HOST}$ENV{DOCUMENT_URI}";
 
     # questions;
     # is startDate ok without the time? might need a different table with FK
@@ -227,7 +227,7 @@ sub generate_jsonld {
         "\@context":"http://schema.org",
         "\@type":"Organization",
         "name":"Bay Areay Country Dance Society",
-        "url":"http://www.bacds.org/"
+        "url":"https://www.bacds.org/"
     },
     "location":
     {
