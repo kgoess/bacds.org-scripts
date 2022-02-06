c06739313f16d0cc9a93e0bb2f093e331b48640f
Kevin M. Goess <kevin@goess.org>
2022-02-06 09:26:05 -0800

    Convert getleaders to Model::Event

--- scripts/getleaders.pl
+++ scripts/getleaders.pl
@@ -3,118 +3,43 @@
 ## public_html/dancefinder/content.html:<!--#include virtual="/scripts/getleaders.pl" -->
 ##
 ## note that this script doesn't look at QUERY_STRING
+
 use strict;
 use Time::Local;
 use Date::Calc qw(Day_of_Week Week_Number Day_of_Year);
 use DBI;
 
+use bacds::Model::Event;
+
 my $CSV_DIR = $ENV{TEST_CSV_DIR} || '/var/www/bacds.org/public_html/data';
 my $TEST_TODAY = $ENV{TEST_TODAY};
 
-my ($dbh, $loc_dbh, $sth, $loc_sth);
-my ($startday,$endday,$type,$loc,$leader,$band,$comments);
-my ($today_year,$today_mon,$today_day,$today, $today_sec);
-my ($last_year,$last_mon,$last_day,$last, $last_sec);
-my ($qrystr, $loc_qry, $loc_type);
-my ($style, $venue, $numdays);
-my ($styleurl, $danceurl);
-my ($outputtype);
-my (%leaderhash);
-my @vals;
-my @clst;
-my ($i, $varname, $data);
-my @monlst = (
-	"January",
-	"February",
-	"March",
-	"April",
-	"May",
-	"June",
-	"July",
-	"August",
-	"September",
-	"October",
-	"November",
-	"December"
-);
-my ($yr, $mon, $day);
-my ($key, $hall, $addr, $city, $ven_comment);
-my ($loc_hall, $loc_addr, $loc_city, $loc_ven_comment);
-
-##
-## Grab arguments
-##
-#@vals = split(/&/,$ENV{'QUERY_STRING'}) if $ENV{'QUERY_STRING'};
-#foreach $i (@vals) {
-#	($varname, $data) = split(/=/,$i);
-#	$style = $data if $varname eq "style";
-#	$venue = $data if $varname eq "venue";
-#	$numdays = $data if $varname eq "numdays";
-#	$outputtype = $data if $varname eq "outputtype";
-#}
 
 ##
 ## First, get the current date.
 ##
-($today_day, $today_mon, $today_year) = my_localtime();
-$today_sec = timelocal(0,0,0,$today_day,$today_mon,$today_year);
-$today_mon++;
-$today_mon = sprintf "%02d", $today_mon;
-$today_day = sprintf "%02d", $today_day;
-$today_year += 1900;
-$today = "$today_year-$today_mon-$today_day";
+my ($today_day, $today_mon, $today_year) = my_localtime();
+my $today = "$today_year-$today_mon-$today_day";
 
-if ($numdays) {
-	##
-	## Now figure out what the date will be in a week.
-	##
-	$last_sec = $today_sec + (86400 * 8);
-	($last_day, $last_mon, $last_year) = (localtime($last_sec))[3,4,5];
-	$last_mon++;
-	$last_mon = sprintf "%02d", $last_mon;
-	$last_day = sprintf "%02d", $last_day;
-	$last_year += 1900;
-	$last = "$last_year-$last_mon-$last_day";
 
-#print "Today is $today\n";
-#print "It will be $last in $numdays days\n";
-}
-
-##
-## Now build our query string
-##
-$qrystr = "SELECT * FROM schedule";
-$qrystr .= " WHERE ";
-$qrystr .= "startday >= '" . $today . "'";
-$qrystr .= " AND startday < '" . $last . "'" if $numdays;
-$qrystr .= " AND loc = '" . $venue . "'" if $venue;
-$qrystr .= " AND type LIKE '%" . $style . "%'" if $style;
-$qrystr .= " AND endday IS NULL";
-$qrystr .= " AND leader IS NOT NULL";
-
-#print "$qrystr\n";
-
-##
-## Set up the table and make the query
-##
-$dbh = get_dbh();
-# get schedule info
-$sth = $dbh->prepare($qrystr);
-$sth->execute();
+my @events = bacds::Model::Event->load_all(
+    after => $today,
+    without_end_date => 1,
+    includes_leader => 1,
+);
 
 ##
 ## Pull out caller names
 ##
-#print "content-type: text/html\n\n";
-while (($startday, $endday, $type, $loc, $leader, $band, $comments, $loc_type)
-	 = $sth->fetchrow_array()
-) {
+my (%leaderhash);
+foreach my $event (@events) {
+    my $leader = $event->leader;
 	#
 	# If leader has a location code, nuke it
 	#
 	$leader =~ s/ \[[^\]]+\].*//g;
-	@clst = split(/, /, $leader);
-	foreach $i (@clst) {
+	my @clst = split(/, /, $leader);
+	foreach my $i (@clst) {
 		$leaderhash{$i} = $i if ! $leaderhash{$i};
 	}
 }
@@ -122,7 +47,7 @@ while (($startday, $endday, $type, $loc, $leader, $band, $comments, $loc_type)
 # And print out our resulting leaders
 print "<select name=\"caller\">\n";
 print "    <option value=\"\">ALL LEADERS</option>\n";
-foreach $i (sort keys %leaderhash) {
+foreach my $i (sort keys %leaderhash) {
 	print "    <option value=\"$i\">" . $i . "</option>\n";
 }
 print "</select>\n";
@@ -130,15 +55,10 @@ print "</select>\n";
 sub my_localtime {
     if ($TEST_TODAY) {
         my ($year, $mon, $day) = split '-', $TEST_TODAY;
-        $mon--;
-        if ($mon < 0) {
-            $mon = 11;
-        }
-        $year -= 1900;
         return $day, $mon, $year;
     } else {
-        my ($today_day, $today_mon, $today_year) = (localtime)[3,4,5];
-        return $today_day, $today_mon, $today_year;
+        my $now = DateTime->now;
+        return map { $now->$_ } qw/day month year/;
     }
 }
 
--- t/getleaders.t
+++ t/getleaders.t
@@ -12,11 +12,44 @@ use Test::More tests => 1;
 $ENV{TEST_CSV_DIR} = 't/data/';
 $ENV{TEST_TODAY} = '2018-09-06';
 
-my ($stdout, $stderr, $exit) = capture {
+my $expected;
+my ($stdout, $stderr, $exit);
+
+#
+# getleaders.pl doesn't look at QUERY_STRING so only the one test
+#
+($stdout, $stderr, $exit) = capture {
     do 'scripts/getleaders.pl';
 };
 die "getleaders.pl died: $@ $stderr" if !$exit;
 note $stderr if $stderr;
 
-like $stdout, qr{<option value="Bruce Hamilton">Bruce Hamilton</option>},
-    "looks like getleaders.pl is working";
+$expected = <<EOL;
+<select name="caller">
+    <option value="">ALL LEADERS</option>
+    <option value="6:45&ndash;7:30pm">6:45&ndash;7:30pm</option>
+    <option value="Alan Winston">Alan Winston</option>
+    <option value="Alexandra Deis-Lauby">Alexandra Deis-Lauby</option>
+    <option value="Bruce Hamilton">Bruce Hamilton</option>
+    <option value="Bruce Herbold">Bruce Herbold</option>
+    <option value="Erik Hoffman">Erik Hoffman</option>
+    <option value="FREE! (donations gladly accepted)">FREE! (donations gladly accepted)</option>
+    <option value="Kalia Kliban">Kalia Kliban</option>
+    <option value="Kelsey Hartman">Kelsey Hartman</option>
+    <option value="Lynn Ackerson">Lynn Ackerson</option>
+    <option value="Mary Luckhardt">Mary Luckhardt</option>
+    <option value="Mavis McGaugh">Mavis McGaugh</option>
+    <option value="Patti Cobb">Patti Cobb</option>
+    <option value="Sharon Green">Sharon Green</option>
+    <option value="Sharon Green ">Sharon Green </option>
+    <option value="Steve White">Steve White</option>
+    <option value="Waltz Away Your Worries (free waltzing">Waltz Away Your Worries (free waltzing</option>
+    <option value="Yoyo Zhou">Yoyo Zhou</option>
+    <option value="maybe you?">maybe you?</option>
+    <option value="no teaching) Audrey Knuth">no teaching) Audrey Knuth</option>
+    <option value="tba">tba</option>
+</select>
+EOL
+
+eq_or_diff $stdout, $expected, "with no args";
+
