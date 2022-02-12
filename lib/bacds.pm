c43946e812303f3709cdbab120df9cc78d513d72
Kevin M. Goess <kevin@goess.org>
2022-02-12 07:32:02 -0800

    Checking in Makefile.PL infrastructure
    Now the regular perl incantation will work to install it:
    
        perl Makefile.PL
        make
        make test
        make install

---
+++ lib/bacds.pm
@@ -0,0 +1,33 @@
+=head1 NAME
+
+bacds - code for bacds web server
+
+=head1 DESCRIPTION
+
+This is just a place to hold the $VERSION for bacds. See other modules
+in here for actual details.
+
+=head1 AUTHOR
+
+Kevin M. Goess
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2022 by Kevin M. Goess
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.16.3 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
+
+package bacds;
+
+use 5.016003;
+use strict;
+use warnings;
+
+our $VERSION = '0.01';
+
+
+1;
