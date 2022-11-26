8fa6d9cba5ee340f7d7058a7b9fe8c28e8213002
Kevin M. Goess <kevin@goess.org>
2022-11-26 10:38:12 -0800

    Add DBIx optino to getleaders.pl
    If the DBIX_TEST=1 cookie is set then we'll pull the information from
    bacds::Scheduler DBIx libraries and mysql.

--- scripts/getleaders.pl
+++ scripts/getleaders.pl
@@ -9,36 +9,113 @@
 
 use strict;
 
+use Template;
+use CGI::Cookie;
+
 use bacds::Model::Event;
 use bacds::Utils qw/today_ymd/;
 
-my @events = bacds::Model::Event->load_all(
-    after => today_ymd(),
-    without_end_date => 1,
-    includes_leader => 1,
-);
+use bacds::Scheduler;
+use bacds::Scheduler::Schema;
+use bacds::Scheduler::Util::Db qw/get_dbh/;
 
-##
-## Pull out caller names
-##
-my (%leaderhash);
-foreach my $event (@events) {
-    my $leader = $event->leader;
-	#
-	# If leader has a location code, nuke it
-	#
-	$leader =~ s/ \[[^\]]+\].*//g;
-	my @clst = split(/, /, $leader);
-	foreach my $i (@clst) {
-		$leaderhash{$i} = $i if ! $leaderhash{$i};
-	}
+main();
+
+sub main {
+
+    my ($caller_names, $needs_html_escaping);
+
+    if (get_dbix_test_cookie()) {
+        ($caller_names, $needs_html_escaping) = get_leaders_dbix();
+    } else {
+        ($caller_names, $needs_html_escaping)= get_leaders_csv();
+    }
+
+    print_results($caller_names, $needs_html_escaping);
 }
-#
-# And print out our resulting leaders
-print "<select name=\"caller\">\n";
-print "    <option value=\"\">ALL LEADERS</option>\n";
-foreach my $i (sort keys %leaderhash) {
-	print "    <option value=\"$i\">" . $i . "</option>\n";
+
+sub get_dbix_test_cookie {
+    my %cookies;
+
+    if ($ENV{COOKIE}) { # unit tests
+        %cookies = CGI::Cookie->parse($ENV{COOKIE});
+    } else {
+        %cookies = CGI::Cookie->fetch;
+    }
+
+    if (my $test_cookie = $cookies{DBIX_TEST}) {
+        return $test_cookie->value;
+    }
+    return;
 }
-print "</select>\n";
+
+
+sub get_leaders_csv {
+    my @events = bacds::Model::Event->load_all(
+        after => today_ymd(),
+        without_end_date => 1,
+        includes_leader => 1,
+    );
+
+    my (%leaderhash);
+    foreach my $event (@events) {
+        my $leader = $event->leader;
+        #
+        # If leader has a location code, nuke it
+        #
+        $leader =~ s/ \[[^\]]+\].*//g;
+        my @clst = split(/, /, $leader);
+        foreach my $i (@clst) {
+            $leaderhash{$i} = $i if ! $leaderhash{$i};
+        }
+    }
+    my @leader_names = sort keys %leaderhash;
+    my $needs_html_escaping = 0;
+    return \@leader_names, $needs_html_escaping;
+}
+
+sub get_leaders_dbix {
+    my $dbh = get_dbh();
+
+    my $rs = $dbh->resultset('Event')->search({
+            start_date => { '>=' => today_ymd },
+    });
+
+    my %leaderhash;
+    while (my $event = $rs->next) {
+        my $callers = $event->callers
+            or next;
+        while (my $caller = $callers->next) {
+            $leaderhash{$caller->name} = 1;
+        }
+    }
+
+    my @leader_names = sort keys %leaderhash;
+    my $needs_html_escaping = 1;
+    return \@leader_names, $needs_html_escaping;
+}
+
+sub print_results {
+    my ($leader_names, $needs_html_escaping) = @_;
+
+    my $tt = Template->new();
+
+    my $escape_html = $needs_html_escaping
+        ? ' | html'
+        : '';
+
+    # the "leader" values are already HTML-escaped
+    my $template = <<EOL;
+<select name="caller">
+    <option value="">ALL LEADERS</option>
+    [%- FOREACH caller IN callers %]
+        <option value="[% caller $escape_html %]">[% caller $escape_html %]</option>
+    [%- END %]
+</select>
+EOL
+    $tt->process(\$template, { callers => $leader_names } )
+        || die $tt->error(), "\n";
+
+}
+
 
